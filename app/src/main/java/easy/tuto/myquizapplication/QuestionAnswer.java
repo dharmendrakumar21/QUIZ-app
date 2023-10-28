ae496285a61973e8aeba79a7e84e459cfcf0ab7a
Dharmendra Kumar <136281464+Dharmendragithubprofile@users.noreply.github.com>
2023-10-28 14:20:23 +0530

    quiz app

---
+++ app/src/main/java/easy/tuto/myquizapplication/QuestionAnswer.java
@@ -0,0 +1,25 @@
+package easy.tuto.myquizapplication;
+
+public class QuestionAnswer {
+    public static String question[] = {
+            "Which is your dream company?",
+            "Which  programming language you like most?",
+            "In which company did you do internship?",
+            "What is your name ?"
+    };
+
+    public static String choices[][] = {
+            {"Microsoft", "Apple", "Flipkart", "Google"},
+            {"C++", "Kotlin", "Java", "Python"},
+            {"BharatIntern", "codeClause", "Octa", "All the above"},
+            {"ak", "Dharma", "aman", "ak"}
+    };
+
+    public static String correctAnswers[] = {
+            "Microsoft",
+            "Java",
+            "All the above",
+            "Dharma ",
+
+    };
+}
